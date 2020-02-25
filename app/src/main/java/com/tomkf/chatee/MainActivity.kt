0a2eabaf1c27b0173a3f3a8c71a9164bdd2a4e61
tomkf <thomaskeithferris@gmail.com>
2020-02-25 10:47:59 -0800

    click listner

--- app/src/main/java/com/tomkf/chatee/MainActivity.kt
+++ app/src/main/java/com/tomkf/chatee/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.tomkf.chatee
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        login_button.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
     }
 }
