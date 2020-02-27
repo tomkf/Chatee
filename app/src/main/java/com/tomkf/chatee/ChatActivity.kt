53e17e640ad8bf0594d0274b655a0d28d5d8a439
tomkf <thomaskeithferris@gmail.com>
2020-02-26 18:25:50 -0800

    message adaptor bound

--- app/src/main/java/com/tomkf/chatee/ChatActivity.kt
+++ app/src/main/java/com/tomkf/chatee/ChatActivity.kt
@@ -15,66 +15,70 @@ import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.activity_chat.*
 import kotlinx.android.synthetic.main.item_message.view.*
 
+
 class ChatActivity: AppCompatActivity() {
     private lateinit var messagesDB: DatabaseReference
-
-    // create an instance level messages collection
     var messages: MutableList<Message> = mutableListOf()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_chat)
-
         messagesDB = FirebaseDatabase.getInstance().getReference("Messages")
-
-        // add the event listener to receive data from Firebase
-        messagesDB.addValueEventListener(object: ValueEventListener {
+        messagesDB.addValueEventListener(object: ValueEventListener{
             override fun onDataChange(dataSnapshot: DataSnapshot) {
                 messages = mutableListOf()
-                dataSnapshot.children.forEach {
+                dataSnapshot.children.forEach{
                     val message = it.getValue(Message::class.java)
-                    if (message != null) {
+                    if(message !=null){
                         messages.add(message)
                     }
                 }
-
                 update()
             }
-
             override fun onCancelled(error: DatabaseError) {
-                // Failed to read values: handle error
+                //handle errors
             }
         })
-
+        message_list.layoutManager = LinearLayoutManager(this)
+        //message_list.adapter = MessagesAdapter(messages, this)
         send_message_button.setOnClickListener {
             val sender = FirebaseAuth.getInstance().currentUser?.email
             val message = message_input.text.toString()
-
             if (sender != null) {
                 saveMessage(sender, message)
             }
         }
-
-        message_list.layoutManager = LinearLayoutManager(this)
     }
-
-    // update method to be called when UI needs to be refreshed
     private fun update(){
-        message_list.adapter = MessagesAdaptor(messages, this)
+        message_list.adapter = MessagesAdapter(messages, this)
     }
-
     private fun saveMessage(sender: String, messageBody: String) {
         val key = messagesDB.push().key
         key ?: return
-
         val message = Message(sender, messageBody)
-
         messagesDB.child(key).setValue(message)
     }
-
-
 }
-class MessagesViewHolder(view: View): RecyclerView.ViewHolder(view)
+private class MessagesAdapter(private val messages: List<Message>, val context: Context): RecyclerView.Adapter<MessagesViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MessagesViewHolder {
+        return MessagesViewHolder(LayoutInflater.from(context).inflate(R.layout.item_message, parent, false))
+    }
+    override fun getItemCount(): Int {
+        return messages.count()
+    }
+    override fun onBindViewHolder(holder: MessagesViewHolder, position: Int) {
+        val message = messages[position]
 
-data class Message(val sender: String, val messageBody: String)
+        holder.itemView.sender_label.text = message.sender
+        holder.itemView.message_body_label.text = message.messageBody
+
+        if (FirebaseAuth.getInstance().currentUser?.email == message.sender) {
+            holder.itemView.sender_image.setImageResource(R.drawable.smile)
+            holder.itemView.message_container.setBackgroundResource(R.drawable.rounded_background1)
+        } else {
+            holder.itemView.sender_image.setImageResource(R.drawable.stars)
+            holder.itemView.message_container.setBackgroundResource(R.drawable.rounded_background2)
+        }
+    }
+}
+class MessagesViewHolder(view: View) : RecyclerView.ViewHolder(view)
+data class Message(val sender: String = "", val messageBody: String = "")
